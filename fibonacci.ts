494007b45bee2fd240a1949deb94144d7d3b69d1
Trickey <vngthnh999@gmail.com>
2023-07-23 22:27:10 +0700

    Fibonacci with Recursion version

--- fibonacci.ts
+++ fibonacci.ts
@@ -1,9 +1,25 @@
+function recursiveFibo(index: number): number {
+  if (index < 2) return index;
+  return recursiveFibo(index - 1) + recursiveFibo(index - 2);
+}
+
 function fibonacci(number: number): number[] {
-  const fibo = [0, 1];
-  for (let i = 2; i < number; i++) {
-    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  const fibo = [];
+  for (let i = 0; i <= number; i++) {
+    fibo.push(recursiveFibo(i));
   }
   return fibo;
 }
 
+// function fibonacci(number: number): number[] {
+//   const fibo = [0, 1];
+//   for (let i = 2; i < number; i++) {
+//     fibo[i] = fibo[i - 1] + fibo[i - 2];
+//   }
+//   return fibo;
+// }
+
 console.log(fibonacci(7));
+console.log(fibonacci(6));
+console.log(fibonacci(1));
+console.log(fibonacci(0));
