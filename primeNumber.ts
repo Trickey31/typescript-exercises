6749d348d5526c62e047cda9ac3e65ab8a2d7797
Trickey <vngthnh999@gmail.com>
2023-07-23 08:53:59 +0700

    Completed Prime Number exercise

---
+++ primeNumber.ts
@@ -0,0 +1,12 @@
+function primeNumber(number: number): boolean {
+  if (number <= 1) return false;
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(primeNumber(4));
+console.log(primeNumber(5));
+console.log(primeNumber(10));
+console.log(primeNumber(15));
